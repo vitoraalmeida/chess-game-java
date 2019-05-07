fa455fcf81f8788fa43fbe69e424b31ee4b25102
Vitor Almeida <pro.vitoralmeida@gmail.com>
2019-05-07 18:25:28 -0300

    ChessException and ChessPosition

---
+++ src/com/chess/ChessException.java
@@ -0,0 +1,11 @@
+package com.chess;
+
+public class ChessException extends RuntimeException{
+    private static final long serialLVersionUID = 1L;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+
+
+}
